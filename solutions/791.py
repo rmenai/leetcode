617bd404a4a5ddab387eb610cc84ad6a55debe5f
Rami Menai <rami.menai@outlook.com>
2024-03-11 18:58:59 +0300

    Day 20 - Custom Sort String

---
+++ solutions/791.py
@@ -0,0 +1,17 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        occurrences = {letter: 0 for letter in order}
+        result = ""
+        for letter in s:
+            if letter in order:
+                occurrences[letter] += 1
+            else:
+                result += letter
+
+        for letter in order:
+            result += letter * occurrences[letter]
+
+        return result
+
+if __name__ == "__main__":
+    print(Solution().customSortString("bcafg", "abcd"))
