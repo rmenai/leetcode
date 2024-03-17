29c439ad231da489918440c04f7b71939593831e
Rami Menai <rami.menai@outlook.com>
2024-03-17 23:17:17 +0300

    Day 26 - Insert Interval

---
+++ solutions/57.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        i = 0
+        res = []
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        while i < n and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
+
+
+if __name__ == "__main__":
+    print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
