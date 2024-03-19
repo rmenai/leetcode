a6b81b2ead32dff8a5b40df9a0c409e5e38f71d2
Rami Menai <rami.menai@outlook.com>
2024-03-19 23:01:17 +0300

    Day 27 - Task Scheduler

---
+++ solutions/621.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+        max_count = 0
+
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
+            max_count = max(max_count, freq[ord(task) - ord('A')])
+
+        time = (max_count - 1) * (n + 1)
+        for f in freq:
+            if f == max_count:
+                time += 1
+
+        return max(len(tasks), time)
+
+
+if __name__ == "__main__":
+    print(Solution().leastInterval(["A", "A", "A", "B", "B", "B"], 2))
