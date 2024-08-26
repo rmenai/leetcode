9781270f75e4e45d0ae035c68f8acd015fbdbebc
Rami Menai <rami.menai@outlook.com>
2024-08-26 18:00:58 +0200

    Auto sync leetcode

--- solutions/1.two-sum.c
+++ solutions/1.two-sum.c
@@ -2,7 +2,8 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-  print()
+int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
+  for (int i = 0; i < numsSize; ++i) {
+  }
 }
 // @leet end
