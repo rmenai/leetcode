9b335522ffbfe95cea553b7e4eb79c4dbd1bdb33
Rami Menai <rami.menai@outlook.com>
2024-09-08 15:10:20 +0200

    Auto sync leetcode

--- solutions/725.split-linked-list-in-parts.c
+++ solutions/725.split-linked-list-in-parts.c
@@ -1,18 +1,55 @@
-// @leet start
+#include <stdlib.h>
+
 struct ListNode {
   int val;
   struct ListNode *next;
 };
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-struct ListNode **splitListToParts(struct ListNode *head, int k,
-                                   int *returnSize) {
+// @leet start
+struct ListNode **splitListToParts(struct ListNode *head, int k, int *returnSize) {
+  struct ListNode **heads = calloc(k, sizeof(struct ListNode));
+  for (int i = 0; i < k; i++) heads[i] = NULL;
 
-  struct ListNode **result = malloc(k * sizeof(struct ListNode));
+  // Determine linked list size
+  int size = 0;
+  struct ListNode *cur = head;
+  while (cur != NULL) {
+    cur = cur->next;
+    size++;
+  }
+
+  int nodeSize = size / k;
+  int leftSize = size - nodeSize * k;
+
+  // Split linked list into k parts
+  cur = head;
+  for (int i = 0; i < k; i++) {
+    int nodeWholeSize = leftSize-- > 0 ? nodeSize + 1 : nodeSize;
+    struct ListNode *nodeCur;
+
+    // Initiate first node
+    if (nodeWholeSize-- > 0) {
+      heads[i] = malloc(sizeof(struct ListNode));
+      heads[i]->val = cur->val;
+      heads[i]->next = NULL;
+
+      nodeCur = heads[i];
+      cur = cur->next;
+    }
+
+    // Add other nodes
+    for (int j = 0; j < nodeWholeSize; j++) {
+      struct ListNode *node = malloc(sizeof(struct ListNode));
+      node->val = cur->val;
+      node->next = NULL;
+
+      nodeCur->next = node;
+      nodeCur = nodeCur->next;
+      cur = cur->next;
+    }
+  }
+
   *returnSize = k;
-
-  return result;
+  return heads;
 }
 // @leet end
