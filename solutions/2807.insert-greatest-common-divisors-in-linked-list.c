4619e8cadfdc936323862c448267f1622a116542
Rami Menai <rami.menai@outlook.com>
2024-09-10 11:01:30 +0200

    Auto sync leetcode

---
+++ solutions/2807.insert-greatest-common-divisors-in-linked-list.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+// @leet start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* insertGreatestCommonDivisors(struct ListNode* head){
+
+}
+// @leet end
