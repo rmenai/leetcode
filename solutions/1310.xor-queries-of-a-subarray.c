2c1eed39dfd3844c65b923962c74e738daaf14b1
Rami Menai <rami.menai@outlook.com>
2024-09-13 09:38:07 +0200

    Auto sync leetcode

--- solutions/1310.xor-queries-of-a-subarray.c
+++ solutions/1310.xor-queries-of-a-subarray.c
@@ -5,6 +5,19 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* xorQueries(int* arr, int arrSize, int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
-    
+  int* answer = malloc(queriesSize * sizeof(int));
+  for (int i = 0; i < queriesSize; i++) {
+    int *query = queries[i];
+    int result = arr[query[0]];
+
+    for (int j = query[0] + 1; j <= query[1]; j++) {
+      result ^= arr[j];
+    }
+
+    answer[i] = result;
+  }
+
+  *returnSize = queriesSize;
+  return answer;
 }
 // @leet end
