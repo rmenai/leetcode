688d97a787b9b7a8fd03e0b1690bf31c5e7ed8c9
Rami Menai <rami.menai@outlook.com>
2024-09-16 13:17:11 +0200

    Auto sync leetcode

--- solutions/539.minimum-time-difference.c
+++ solutions/539.minimum-time-difference.c
@@ -1,7 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 // @leet start
+
+int timeToMinutes(char *time) {
+  int hours, minutes;
+  scanf("%d:%d", &hours, &minutes);
+
+  return hours * 60 + minutes;
+  
+}
+
 int findMinDifference(char** timePoints, int timePointsSize) {
-    
+  for (int i = 0; i < timePointsSize - 1; i++) {
+    int a = timeToMinutes(timePoints[i]);
+
+    for (int j = i + 1; j < timePointsSize; j++) {
+      int b = timeToMinutes(timePoints[j]);
+    }
+  }
 }
 // @leet end
