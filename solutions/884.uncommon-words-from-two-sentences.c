824f7b5f4ede1fe3397e438cf394c789709b94f8
Rami Menai <rami.menai@outlook.com>
2024-09-18 05:51:36 +0200

    Auto sync leetcode

---
+++ solutions/884.uncommon-words-from-two-sentences.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+// @leet start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** uncommonFromSentences(char* s1, char* s2, int* returnSize) {
+    
+}
+// @leet end
