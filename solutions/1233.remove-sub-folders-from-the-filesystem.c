d4fee3a635e5c1331998ea463e2c0d1298de1d83
Rami Menai <rami@menai.me>
2024-10-25 16:46:40 +0200

    Auto sync leetcode

---
+++ solutions/1233.remove-sub-folders-from-the-filesystem.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+// @leet start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** removeSubfolders(char** folder, int folderSize, int* returnSize) {
+    
+}
+// @leet end
