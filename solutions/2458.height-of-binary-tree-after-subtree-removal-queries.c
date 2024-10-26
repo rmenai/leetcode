406ca051e410df25d1d6429761de6c573b04bcf6
Rami Menai <rami@menai.me>
2024-10-27 00:42:49 +0200

    Auto sync leetcode

---
+++ solutions/2458.height-of-binary-tree-after-subtree-removal-queries.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* treeQueries(struct TreeNode* root, int* queries, int queriesSize, int* returnSize) {
+    
+}
+// @leet end
