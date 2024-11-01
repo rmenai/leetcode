9d95494889801fac76fb55f6b74a7bf945bf2c03
Rami Menai <rami@menai.me>
2024-11-01 12:31:45 +0100

    Auto sync leetcode

--- solutions/1957.delete-characters-to-make-fancy-string.c
+++ solutions/1957.delete-characters-to-make-fancy-string.c
@@ -3,27 +3,28 @@
 
 // @leet start
 char* makeFancyString(char* s) {
-  int entryLen = strlen(s);
   int resultLen = 0;
+  int entryLen = strlen(s);
 
   char* result = malloc((entryLen + 1) * sizeof(char));
   char current = s[0];
   int count = 1;
 
   for (int i = 1; i < entryLen; i++) {
+    if (count < 3) {
+      result[resultLen++] = current;
+    }
+
     if (s[i] == current) {
       count++;
-      if (i + 1 < entryLen) continue;
-    };
-
-    if (count >= 2) result[resultLen++] = current;
-    result[resultLen++] = current;
+      continue;
+    }
 
     current = s[i];
     count = 1;
   }
 
-  if (result[resultLen - 1] != current) {
+  if (count < 3) {
     result[resultLen++] = current;
   }
 
