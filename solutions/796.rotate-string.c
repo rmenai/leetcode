f1616cb1c2b9e021e1c336f87216480ddfba86c1
Rami Menai <rami@menai.me>
2024-11-03 22:22:13 +0100

    Auto sync leetcode

--- solutions/796.rotate-string.c
+++ solutions/796.rotate-string.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <string.h>
 
 // @leet start
 bool areEqualFromIndex(char* s, char* goal, int i) {
@@ -31,3 +32,62 @@ bool rotateString(char* s, char* goal) {
   return false;
 }
 // @leet end
+
+void buildKMPTable(char* pattern, int* table, int m) {
+  int j = 0;
+  table[0] = 0;
+
+  for (int i = 1; i < m; i++) {
+    if (pattern[i] == pattern[j]) {
+      j++;
+      table[i] = j;
+    } else if (j > 0) {
+    }
+  }
+}
+
+bool KMPSearch(char* s, char* pattern) {
+  int len_pattern = strlen(pattern);
+  int len_s = strlen(s);
+  int table[len_pattern];
+
+  buildKMPTable(pattern, table, len_pattern);
+
+  int i = 0;
+  int j = -1;
+
+  while (j + 1 < len_pattern || i < len_s) {
+    if (s[i] == pattern[j + 1]) {
+      i++;
+      j++;
+    } else {
+      if (j == -1) {
+        i++;
+      } else {
+        j = table[j];
+      }
+    }
+  }
+
+  if (j + 1 != len_pattern) {
+    return false;
+  }
+
+  return true;
+}
+
+bool rotateString(char* s, char* goal) {
+  int len_s = strlen(s);
+  int len_goal = strlen(goal);
+
+  if (len_s != len_goal) {
+    return false;
+  }
+
+  char s2[2 * len_s + 1];
+
+  strcpy(s2, s);
+  strcpy(s2, s);
+
+  return KMPSearch(s2, goal);
+}
