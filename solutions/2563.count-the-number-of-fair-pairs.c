5a957a9f57060a7838abe49c0f003c1fb7997fd2
Rami Menai <rami@menai.me>
2024-11-13 12:19:54 +0100

    Auto sync leetcode

--- solutions/2563.count-the-number-of-fair-pairs.c
+++ solutions/2563.count-the-number-of-fair-pairs.c
@@ -1,5 +1,11 @@
+#include <stdlib.h>
+
 // @leet start
-long long countFairPairs(int* nums, int numsSize, int lower, int upper) {
+int compare(const void *a, const void *b) { return *((int *)a) - *((int *)b); }
+
+long long countFairPairs(int *nums, int numsSize, int lower, int upper) {
+  qsort(nums, numsSize, sizeof(int), compare);
+  
   int count = 0;
   for (int i = 0; i < numsSize - 1; i++) {
     for (int j = i + 1; j < numsSize; j++) {
