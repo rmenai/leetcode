f7ebdb4ad30e8216473f9d0ffcaab1815c779499
Rami Menai <rami@menai.me>
2024-11-15 23:21:05 +0100

    Auto sync leetcode

--- solutions/1574.shortest-subarray-to-be-removed-to-make-array-sorted.c
+++ solutions/1574.shortest-subarray-to-be-removed-to-make-array-sorted.c
@@ -1,19 +1,27 @@
-#include <stdlib.h>
 #include <stdbool.h>
 
 // @leet start
 int findLengthOfShortestSubarray(int* arr, int arrSize) {
-  bool isSorted = false;
+  int right = arrSize - 1;
+  while (right > 0 && arr[right] >= arr[right - 1]) {
+    right--;
+  }
 
-  int i = 1;
-  int j = 0;
-
-  while (!isSorted) {
-    if (arr[i] > arr[i - 1]) {
-      i++;
-      continue;
+  int answer = right;
+  int left = 0;
+  while (left < right && (left == 0 || arr[left - 1] <= arr[left])) {
+    while (right < arrSize && arr[left] > arr[right]) {
+      right++;
     }
 
+    int distance = right - left - 1;
+    if (distance < answer) {
+      answer = distance;
+    }
+
+    left++;
   }
+
+  return answer;
 }
 // @leet end
