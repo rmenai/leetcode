1061bc0fa1adcd46830444fb453d13876e507f81
Rami Menai <rami@menai.me>
2024-11-24 00:41:58 +0100

    Auto sync leetcode

--- solutions/1861.rotating-the-box.c
+++ solutions/1861.rotating-the-box.c
@@ -1,21 +1,51 @@
 #include <stdlib.h>
 
 // @leet start
+const char OBSTACLE = '*';
+const char STONE = '#';
+const char AIR = '.';
 
-
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-char** rotateTheBox(char** box, int boxSize, int* boxColSize, int* returnSize, int** returnColumnSizes){
+char** rotateTheBox(char** box, int boxSize, int* boxColSize, int* returnSize, int** returnColumnSizes) {
+  char** reversedBox = (char**)malloc(boxColSize[0] * sizeof(char*));
   *returnSize = boxColSize[0];
-  *returnColumnSizes = (int *)malloc(boxSize * sizeof(int));
 
-  int **reversedBox = (int **)malloc(boxColSize[0] * sizeof(int *));
+  *returnColumnSizes = (int*)malloc(boxColSize[0] * sizeof(int));
+  for (int i = 0; i < boxColSize[0]; i++) {
+    reversedBox[i] = (char*)malloc(boxSize * sizeof(char));
+    (*returnColumnSizes)[i] = boxSize;
 
-  for (int i = 0; i < *returnSize; i++) {
-    reversedBox[i] = (int *)malloc(( * sizeof(int));
+    for (int j = 0; j < boxSize; j++) {
+      reversedBox[i][j] = AIR;
+    }
   }
+
+  for (int i = 0; i < boxSize; i++) {
+    int stoneCount = 0;
+    int k = boxColSize[i] - 1;
+    for (int j = boxColSize[i] - 1; j >= 0; j--) {
+      if (box[i][j] == OBSTACLE) {
+        for (int _ = 0; _ < stoneCount; _++) {
+          reversedBox[k][boxSize - i - 1] = STONE;
+          k--;
+        }
+
+        k = j;
+        reversedBox[k--][boxSize - i - 1] = OBSTACLE;
+        stoneCount = 0;
+        continue;
+      }
+
+      if (box[i][j] == STONE) {
+        stoneCount++;
+      }
+    }
+
+    for (int _ = 0; _ < stoneCount; _++) {
+      reversedBox[k][boxSize - i - 1] = STONE;
+      k--;
+    }
+  }
+
+  return reversedBox;
 }
 // @leet end
