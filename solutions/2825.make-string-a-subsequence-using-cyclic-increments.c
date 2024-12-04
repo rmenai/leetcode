51cabe216ee9bee03e3c8bf490bc44f3d6a4fc65
Rami Menai <rami@menai.me>
2024-12-04 20:05:22 +0100

    Auto sync leetcode

---
+++ solutions/2825.make-string-a-subsequence-using-cyclic-increments.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+// @leet start
+bool canMakeSubsequence(char* str1, char* str2) {
+  int *letterCount = (int *)malloc(26 * sizeof(int));
+  for (int i = 0; str1[i] != '\0'; i++) {
+    letterCount
+  }
+}
+// @leet end
