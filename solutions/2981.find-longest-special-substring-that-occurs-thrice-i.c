c09dc7910dab43f4bc27a582e32583065952e9bc
Rami Menai <rami@menai.me>
2024-12-10 21:57:43 +0100

    Auto sync leetcode

--- solutions/2981.find-longest-special-substring-that-occurs-thrice-i.c
+++ solutions/2981.find-longest-special-substring-that-occurs-thrice-i.c
@@ -1,7 +1,40 @@
-#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 // @leet start
 int maximumLength(char* s) {
-    
+  int length = strlen(s);
+  int frequency[26];
+
+  for (int k = length - 2; k >= 1; k--) {
+    int iterations = length - k + 1;
+
+    for (int i = 0; i < 26; i++) {
+      frequency[i] = 0;
+    }
+
+    for (int i = 0; i < iterations; i++) {
+      bool brocken = false;
+      char c = s[i];
+      for (int j = i; j < k + i; j++) {
+        if (s[j] != c) {
+          brocken = true;
+          break;
+        }
+      }
+
+      if (!brocken) {
+        frequency[c - 'a']++;
+      }
+    }
+
+    for (int i = 0; i < 26; i++) {
+      if (frequency[i] >= 3) {
+        return k;
+      }
+    }
+  }
+
+  return -1;
 }
 // @leet end
