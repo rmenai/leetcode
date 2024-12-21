c1da94cf90547d469843e532544296ea6a90d7f8
Rami Menai <rami@menai.me>
2024-12-21 19:03:58 +0100

    Auto sync leetcode

---
+++ solutions/2872.maximum-number-of-k-divisible-components.rs
@@ -0,0 +1,12 @@
+// @leet start
+impl Solution {
+    pub fn max_k_divisible_components(
+        n: i32,
+        edges: Vec<Vec<i32>>,
+        values: Vec<i32>,
+        k: i32,
+    ) -> i32 {
+    }
+}
+// @leet end
+
