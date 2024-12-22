12f862260855e1e3b9f12ec1a2b6083e31174ce0
Rami Menai <rami@menai.me>
2024-12-23 00:40:46 +0100

    Auto sync leetcode

--- solutions/2940.find-building-where-alice-and-bob-can-meet.py
+++ solutions/2940.find-building-where-alice-and-bob-can-meet.py
@@ -1,78 +1,33 @@
-import heapq
-
 # @leet start
-
-
-class OrderedHashSet:
-    def __init__(self):
-        self.node_map = {}
-        self.order = DoublyLinkedList()
-
-    def add(self, key):
-        if key not in self.node_map:
-            node = self.order.append(key)
-            self.node_map[key] = node
-
-    def remove(self, key):
-        if key in self.node_map:
-            node = self.node_map[key]
-            self.order.remove(node)
-            del self.node_map[key]
-
-    def traverse(self):
-        return self.order.to_list()
-
-
-class DoublyLinkedList:
-    class Node:
-        def __init__(self, value):
-            self.value = value
-            self.prev = None
-            self.next = None
-
-    def __init__(self):
-        self.head = self.Node(None)
-        self.tail = self.Node(None)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def append(self, value):
-        node = self.Node(value)
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
-        return node
-
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node
-
-    def to_list(self):
-        result = []
-        current = self.head.next
-        while current != self.tail:
-            result.append(current.value)
-            current = current.next
-        return result
+from sortedcontainers import SortedList
+import heapq
 
 
 class Solution:
     def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        ordered_set = OrderedHashSet()
-        for i in range(len(heights)):
-            ordered_set.add(i)
+        ordered_set = SortedList(range(len(heights)))
 
         h = [(heights[i], i) for i in range(len(heights))]
-
         heapq.heapify(h)
-        queries.sort(key=lambda a: max(heights[a[0]], heights[a[1]]))
 
-        for i, j in queries:
+        q = [(queries[i], i) for i in range(len(queries))]
+        q.sort(key=lambda a: max(heights[a[0][0]], heights[a[0][1]]))
+
+        result = [-1 for _ in range(len(queries))]
+
+        for query, index in q:
+            i, j = query
             m = max(i, j, key=lambda a: heights[a])
             while h[0][0] < heights[m]:
                 _, k = heapq.heappop(h)
                 ordered_set.remove(k)
+
+            # return list(ordered_set)
+            pos = ordered_set.bisect_right(m)
+            if pos < len(ordered_set):
+                result[index] = ordered_set[pos]
+
+        return result
 
 
 # @leet end
