545e24f8ed9b7cc30de28edf3fe1e1a1f670f33e
Rami Menai <rami@menai.me>
2024-12-23 10:14:19 +0100

    Auto sync leetcode

---
+++ solutions/2471.minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# @leet start
+class Solution:
+    def minimumOperations(self, root: Optional[TreeNode]) -> int:
+        pass
+
+
+# @leet end
+
