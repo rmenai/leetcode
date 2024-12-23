1b79064da6fdd0ba5e9d746ae46952fb84f22ea4
Rami Menai <rami@menai.me>
2024-12-23 10:25:32 +0100

    Auto sync leetcode

--- solutions/2471.minimum-number-of-operations-to-sort-a-binary-tree-by-level.rs
+++ solutions/2471.minimum-number-of-operations-to-sort-a-binary-tree-by-level.rs
@@ -6,7 +6,7 @@
 //   pub left: Option<Rc<RefCell<TreeNode>>>,
 //   pub right: Option<Rc<RefCell<TreeNode>>>,
 // }
-// 
+//
 // impl TreeNode {
 //   #[inline]
 //   pub fn new(val: i32) -> Self {
@@ -17,11 +17,12 @@
 //     }
 //   }
 // }
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 impl Solution {
     pub fn minimum_operations(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        
+        pr
     }
 }
 // @leet end
+
