8c500f2e7abe68433c08e874ede563b01263c79b
Rami Menai <rami@menai.me>
2024-12-26 12:19:31 +0100

    Auto sync leetcode

--- solutions/494.target-sum.py
+++ solutions/494.target-sum.py
@@ -5,13 +5,32 @@ from typing import List
 # @leet start
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        length = len(nums)
+
+        nums.sort(reverse=True)
+
+        prefix = [1000 for _ in range(length)]
+        prefix[length - 1] = nums[length - 1]
+
+        for i in range(length - 2, -1, -1):
+            prefix[i] = prefix[i + 1] + nums[i]
+
         candidates = deque()
+        candidates.append((-1, -target))
 
-        candidates.append(nums[0])
-        candidates.append(-nums[0])
+        count = 0
+        while candidates:
+            i, candidate = candidates.popleft()
 
-        while candidates"
+            if i == length - 1:
+                count += 1 if candidate == 0 else 0
+                continue
+
+            if abs(candidate) <= prefix[i + 1]:
+                candidates.append((i + 1, candidate + nums[i + 1]))
+                candidates.append((i + 1, candidate - nums[i + 1]))
+
+        return count
 
 
 # @leet end
-
