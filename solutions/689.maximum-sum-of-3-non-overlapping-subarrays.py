888a10dbe9a64d0f1aa2b8942a7ca6f33dcdd249
Rami Menai <rami@menai.me>
2024-12-28 23:51:39 +0100

    Auto sync leetcode

---
+++ solutions/689.maximum-sum-of-3-non-overlapping-subarrays.py
@@ -0,0 +1,11 @@
+from heapq import heapify, heappop, heappush
+
+
+# @leet start
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+
+# @leet end
+
---
+++ solutions/689.maximum-sum-of-3-non-overlapping-subarrays.rs
@@ -0,0 +1,7 @@
+// @leet start
+impl Solution {
+    pub fn max_sum_of_three_subarrays(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        
+    }
+}
+// @leet end
