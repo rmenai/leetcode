bb7ea8fef887bc74e82c993adb5257cf1c955b6b
Rami Menai <rami@menai.me>
2025-01-02 11:04:15 +0100

    Auto sync leetcode

--- solutions/2559.count-vowel-strings-in-ranges.py
+++ solutions/2559.count-vowel-strings-in-ranges.py
@@ -5,23 +5,19 @@ from typing import List
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
         VOYELS = {"a", "e", "i", "o", "u"}
-        voyels = set()
+        prefix_sum = [1 if words[0][0] in VOYELS and words[0][-1] in VOYELS else 0]
 
-        for word in words:
-            if word[0] in VOYELS and word[-1] in VOYELS:
-                voyels.add(word)
+        for i in range(1, len(words)):
+            prefix_sum.append(prefix_sum[i - 1])
+            if words[i][0] in VOYELS and words[i][-1] in VOYELS:
+                prefix_sum[i] += 1
 
         result = []
         for i, j in queries:
-            add = 0
-            for k in range(i, j + 1):
-                if words[k] in voyels:
-                    add += 1
-
-            result.append(add)
+            remove = 0 if i - 1 < 0 else prefix_sum[i - 1]
+            result.append(prefix_sum[j] - remove)
 
         return result
 
 
 # @leet end
-
