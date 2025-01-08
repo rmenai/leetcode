dcbc5fe5f86ab4fe91f56c9148698189b4ca2d3f
Rami Menai <rami@menai.me>
2025-01-08 18:00:11 +0100

    Auto sync leetcode

--- solutions/3042.count-prefix-and-suffix-pairs-i.rs
+++ solutions/3042.count-prefix-and-suffix-pairs-i.rs
@@ -1,7 +1,17 @@
 // @leet start
 impl Solution {
     pub fn count_prefix_suffix_pairs(words: Vec<String>) -> i32 {
-        
+        let mut count = 0;
+        for i in 0..(words.len() - 1) {
+            for j in (i + 1)..words.len() {
+                if words[j].starts_with(&words[i]) && words[j].ends_with(&words[i]) {
+                    count += 1;
+                }
+            }
+        }
+
+        count
     }
 }
 // @leet end
+
