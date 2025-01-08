252e2474484bc2ccb338fac61675d1aa53d71b29
Rami Menai <rami@menai.me>
2025-01-08 17:39:50 +0100

    Auto sync leetcode

--- solutions/204.count-primes.c
+++ solutions/204.count-primes.c
@@ -2,18 +2,18 @@
 
 // @leet start
 int countPrimes(int n) {
-  int *primes = (int *)calloc(n + 1, sizeof(int));
+  int *primes = (int *)calloc(n, sizeof(int));
 
-  for (int i = 2; i <= n; i++) {
+  for (int i = 2; i < n; i++) {
     if (primes[i] == 0) {
-      for (int j = 2 * i; j <= n; j += i) {
+      for (int j = 2 * i; j < n; j += i) {
         primes[j] = 1;
       }
     }
   }
 
   int count = 0;
-  for (int i = 2; i <= n; i++) {
+  for (int i = 2; i < n; i++) {
     if (primes[i] == 0) {
       count++;
     }
---
+++ solutions/3042.count-prefix-and-suffix-pairs-i.rs
@@ -0,0 +1,7 @@
+// @leet start
+impl Solution {
+    pub fn count_prefix_suffix_pairs(words: Vec<String>) -> i32 {
+        
+    }
+}
+// @leet end
