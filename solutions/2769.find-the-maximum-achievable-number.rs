7cffb936ddda2cb10d0975ffd4889fa49f74db44
Rami Menai <rami@menai.me>
2025-01-17 23:14:17 +0100

    Auto sync leetcode

---
+++ solutions/2769.find-the-maximum-achievable-number.rs
@@ -0,0 +1,8 @@
+// @leet start
+impl Solution {
+    pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+        num + 2 * t
+    }
+}
+// @leet end
+
---
+++ solutions/3110.score-of-a-string.rs
@@ -0,0 +1,11 @@
+// @leet start
+impl Solution {
+    pub fn score_of_string(s: String) -> i32 {
+        s.as_bytes()
+            .windows(2)
+            .map(|w| (w[0] as i32 - w[1] as i32).abs())
+            .sum()
+    }
+}
+// @leet end
+
