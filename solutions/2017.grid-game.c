672f18e96cf9206130f2664fa80a3eae0d26ab63
Rami Menai <rami@menai.me>
2025-01-21 11:00:45 +0100

    Auto sync leetcode

--- solutions/2017.grid-game.c
+++ solutions/2017.grid-game.c
@@ -4,10 +4,10 @@
 // @leet start
 long long gridGame(int **grid, int gridSize, int *gridColSize) {
   int columns = gridColSize[0];
-  int **prefixSum = (int **)malloc(2 * sizeof(int *));
+  long long **prefixSum = (long long **)malloc(2 * sizeof(long long *));
 
   for (int i = 0; i < 2; i++) {
-    prefixSum[i] = (int *)calloc(columns, sizeof(int));
+    prefixSum[i] = (long long *)calloc(columns, sizeof(long long));
   }
 
   prefixSum[0][0] = grid[0][0];
@@ -18,13 +18,13 @@ long long gridGame(int **grid, int gridSize, int *gridColSize) {
     prefixSum[1][j] = prefixSum[1][j - 1] + grid[1][j];
   }
 
-  int min = INT_MAX;
+  long long min = LONG_MAX;
 
   for (int i = 0; i < columns; i++) {
-    int sum1 = prefixSum[0][columns - 1] - prefixSum[0][i];
-    int sum2 = i > 0 ? prefixSum[1][i - 1] : 0;
+    long long sum1 = prefixSum[0][columns - 1] - prefixSum[0][i];
+    long long sum2 = i > 0 ? prefixSum[1][i - 1] : 0;
 
-    int max = sum1 > sum2 ? sum1 : sum2;
+    long long max = sum1 > sum2 ? sum1 : sum2;
     min = max < min ? max : min;
   }
 
