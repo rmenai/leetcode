87787c7a9b6b2e4abe3023f66757f5942c70667a
Rami Menai <rami@menai.me>
2025-03-05 00:22:17 +0100

    Auto sync leetcode

---
+++ solutions/1780.check-if-number-is-a-sum-of-powers-of-three.rs
@@ -0,0 +1,15 @@
+// @leet start
+impl Solution {
+    pub fn check_powers_of_three(mut n: i32) -> bool {
+        while n != 0 {
+            if n % 3 == 2 {
+                return false;
+            }
+            n /= 3;
+        }
+
+        true
+    }
+}
+// @leet end
+
